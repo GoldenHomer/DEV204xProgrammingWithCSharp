b7eba28499c04a009e6d24eb09f3f9f1d91dee79
Jose Contreras <jocontre@joselcontreras.com>
2015-04-28 13:33:50 -0500

    Update structs.cs

--- Module 4/structs.cs
+++ Module 4/structs.cs
@@ -12,5 +12,13 @@ public namespace Program {
     enum Days {Sat, Sun, Mon, Tue, Wed, Thu, Fri};
     enum Months {Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
     int[] Numbers = new int[5];
+    
+    for(int i = 0; Numbers.Length; i++){
+      Numbers[i] = i;
+    }
+    
+    while(i < Numbers.Length) {
+      Console.WriteLine("Element in index {0} of Numbers array is {1}", i, Numbers[i]);
+    }
   }
 }

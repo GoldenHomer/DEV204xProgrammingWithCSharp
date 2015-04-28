0c487b7535ecc9834028c19dc644d90682591ca3
Jose Contreras <jocontre@joselcontreras.com>
2015-04-28 12:49:04 -0500

    Update structs.cs

--- Module 4/structs.cs
+++ Module 4/structs.cs
@@ -1,6 +1,16 @@
-public struct Student {
-  public string firstName;
-  public string lastName;
-  public string address;
-  DateTime birth;
+using System;
+
+public namespace Program {
+  public struct Student {
+    public string firstName;
+    public string lastName;
+    public string address;
+    DateTime birth;
+  }
+  
+  public static void Main(string[] args) {
+    enum Days {Sat, Sun, Mon, Tue, Wed, Thu, Fri};
+    enum Months {Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+    int[] Numbers = new int[5];
+  }
 }

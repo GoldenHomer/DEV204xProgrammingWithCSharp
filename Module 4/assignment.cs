8d967869b1fe258027c721931403fb69c9588565
Jose Contreras <jocontre@joselcontreras.com>
2015-04-28 21:29:42 -0500

    Update assignment.cs

--- Module 4/assignment.cs
+++ Module 4/assignment.cs
@@ -1,1 +1,19 @@
+using System;
 
+public struct Student {
+  
+} 
+
+public struct Professor {
+  
+}
+
+public struct Program {
+  
+}
+
+public struct Course {
+  
+}
+
+string[] students = new string[5];

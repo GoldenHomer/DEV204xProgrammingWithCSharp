a29a3260fb37cda718c9cc2d0677635e300cb4d4
Jose Contreras <jocontre@joselcontreras.com>
2015-04-29 18:59:24 -0500

    Update assignment.cs

--- Module 4/assignment.cs
+++ Module 4/assignment.cs
@@ -26,4 +26,23 @@ class Program {
     students[0].lastName = "Man";
     students[0].degree = "Women's Studies";
   }
+  Console.WriteLine(students[0].firstName);
+  Console.WriteLine(students[0].lastName);
+  Console.WriteLine(students[0].degree);
+  
+  for(int i = 0; i < students.Length; i++) {
+    Console.WriteLine("Enter student's first name");
+    arr[i].firstName = Console.ReadLine();
+    Console.WriteLine("Enter student's last name");
+    arr[i].lastName = Console.ReadLine();
+    Console.WriteLine("and choice of major.");
+    arr[i].degree = Console.ReadLine();
+  }
+
+for (int j = 0; j < students.Length; j++) {
+  Console.WriteLine(students[j].firstName);
+  Console.WriteLine(students[j].lastName);
+  Console.WriteLine(students[j].degree);
 }
+
+

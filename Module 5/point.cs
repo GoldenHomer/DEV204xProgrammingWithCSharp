ad50972ebbcde91e02377bdc26a3e6be78721ab4
Jose Contreras <jocontre@joselcontreras.com>
2015-05-05 11:36:33 -0500

    Update point.cs

--- Module 5/point.cs
+++ Module 5/point.cs
@@ -6,6 +6,29 @@ namespace Classes
 {
     class Point
     {
-        // TODO:
+        private int x;
+        private int y;
+        private static int objectCount = 0;
+        
+       // Default constructor
+       public Point() {
+           Console.WriteLine("default constructor called");
+           objectCount++;
+       }
+       
+       // Non-default
+       public Point(int x, int y) {
+           this.x = Point.x;
+           this.y = Point.y;
+       }
+       
+       public void PrintCoords() {
+           Console.WriteLine(this.x);
+           Console.WriteLine(this.y);
+       }
+       
+       public static int ObjectCount() {
+           return objectCount;
+       }
     }
 }

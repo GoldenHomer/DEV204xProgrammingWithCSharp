43d55e50bfe1510c3cb6f9ac812c96dc1618df76
Jose Contreras <jocontre@joselcontreras.com>
2015-05-05 11:43:57 -0500

    Update program.cs

--- Module 5/program.cs
+++ Module 5/program.cs
@@ -13,8 +13,10 @@ namespace Classes
         static void doWork()
         {
             Point origin = new Point();
+            Point bottomRight = new Point(1366, 768);
             Point coord1 = new Point();
             Point coord2 = new Point();
+            Console.WriteLine(Point.ObjectCount());
         }
 
         static void Main(string[] args)
@@ -27,8 +29,6 @@ namespace Classes
             {
                 Console.WriteLine(ex.Message);
             }
-            
-            Point bottomRight = new Point(1366, 768);
         }
     }
 }

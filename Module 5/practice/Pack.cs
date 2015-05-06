00c0db67e62711b5b75b0bc0aeec5858a179085a
Jose Contreras <jocontre@joselcontreras.com>
2015-05-06 11:25:01 -0500

    Update Pack.cs

--- Module 5/practice/Pack.cs
+++ Module 5/practice/Pack.cs
@@ -4,32 +4,57 @@ namespace Cards
 {
 	class Pack
 	{
-        public const int NumSuits = 4;
-        public const int CardsPerSuit = 13;
-        private PlayingCard[,] cardPack;
-        private Random randomCardSelector = new Random();
-
+	        public const int NumSuits = 4;
+	        public const int CardsPerSuit = 13;
+	        private PlayingCard[,] cardPack;
+	        private Random randomCardSelector = new Random();
+	
 		public Pack()
 		{
-            // TODO: initialize the pack of cards
+	            	this.cardPack = new PLayingCard[NumSuits, CardsPerSuit];
+	            		
+	            	for(Suit suit = Suit.Clubs; suit <= Suit.Spades; suit++){
+	            		for(Value value = Value.Two; value <= Value.Ace; value++){
+	            			this.cardPack[(int)suit, (int)value] = new PlayingCard(suit, value);
+	            		}
+	            	}
 		}
-
-        public PlayingCard DealCardFromPack()
-        {
-            // TODO: pick a random card, remove it from the pack, and return it
-            throw new NotImplementedException("DealCardFromPack - TBD");
-        }
-
-        private bool IsSuitEmpty(Suit suit)
-        {
-            // TODO: return true if there are no more cards available in this suit
-            throw new NotImplementedException("IsSuitEmpty - TBD");
-        }
-
-        private bool IsCardAlreadyDealt(Suit suit, Value value)
-        {
-            // TODO: return true if this card has already been dealt   
-            throw new NotImplementedException("IsCardAlreadyDealt - TBD");
-        }
+	
+	        public PlayingCard DealCardFromPack()
+	        {
+	            Suit suit = (Suit)randomCardSelector.Next(NumSuits);
+	            
+	            while(this.IsSuitEmpty(suit)) {
+	            	suit = (Suit)randomCardSelector.Next(NumSuits);
+	            }
+	            
+	            Value value = (Value)randomCardSelector.Next(CardsPerSuit);
+	            while(this.IsCardAlreadyDealt(suit, value)){
+	            	value = (Value)randomCardSelector.Next(CardsPerSuit);
+	            }
+	            
+	            PlayingCard card = this.cardPack[(int)suit, (int)value]; 
+		    this.cardPack[(int)suit, (int)value] = null; 
+		    return card;
+	        }
+	
+	        private bool IsSuitEmpty(Suit suit)
+	        {
+	            bool result = true; 
+		    for (Value value = Value.Two; value <= Value.Ace; value++){ 
+     			if (!IsCardAlreadyDealt(suit, value)) { 
+               			result = false; 
+               			break;
+     			}
+		    } 
+		    return result;
+		    return (this.cardPack[(int)suit, (int)value] == null);
+	        }
+	
+	        private bool IsCardAlreadyDealt(Suit suit, Value value)
+	        {
+	            // TODO: return true if this card has already been dealt   
+	            throw new NotImplementedException("IsCardAlreadyDealt - TBD");
+	        }
 	}
 }

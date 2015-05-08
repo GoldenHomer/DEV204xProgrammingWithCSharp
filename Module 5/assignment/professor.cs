ca1ecc3149a7539c69768b5755527a5484ac5be3
Jose Contreras <jocontre@joselcontreras.com>
2015-05-07 20:04:48 -0500

    Create professor.cs

---
+++ Module 5/assignment/professor.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Prof {
+  class Professor {
+    string profFirst;
+    string profLast;
+    DateTime profBirth;
+    string profAddress1;
+    string profAddress2;
+    string profCity;
+    string profState;
+    int profZip;
+    string profCountry
+  }
+}

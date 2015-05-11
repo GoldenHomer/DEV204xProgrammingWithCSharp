b71fd67e0f457e6c6e429d319da6b92f846f382e
Jose Contreras <jocontre@joselcontreras.com>
2015-05-11 12:53:46 -0500

    Update vehicle.cs

--- Module 6/practice/inheritance/vehicle.cs
+++ Module 6/practice/inheritance/vehicle.cs
@@ -1,1 +1,42 @@
+using System;
 
+namespace Vehicles
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // TODO:
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+        
+        public void StartEngine(string noiseToMakeWhenStarting) {
+          Console.WriteLine("Starting engine: {0}", noiseToMakeWhenStarting);
+        }
+        
+        public void StopEngine(string noiseToMakeWhenStopping) {
+          Console.WriteLine("Stopping engine: {0}", noiseToMakeWhenStopping);
+        }
+    }
+  
+  class Airplane:Vehicle {
+    public void TakeOff() {
+      Console.WriteLine("Take off");
+    }
+    
+    public void land() {
+      Console.WriteLine("Landing");
+    }
+  }
+}

4c4b05f65d2b617ee2d0fc15c1d86b9d9f21ea48
Jose Contreras <jocontre@joselcontreras.com>
2015-05-12 12:20:12 -0500

    Update person.cs

--- Module 6/assignment/person.cs
+++ Module 6/assignment/person.cs
@@ -11,6 +11,25 @@ namespace Person
           int weight;
           string height;
           string birthday;
+          
+          Student Huey = new student();
+        Student Dewey = new student();
+        Student Louie = new student();
+        
+        Course ProgrammingWithC# = new Course();
+        ProgrammingWithC#.name = "Programming with C#";
+        
+        Professor Munsley = new Prof();
+        
+        DegreeProgram IT = new DegreeProgram();
+        IT.programName = "Information Technology";
+        
+        Degree BS = new Degree();
+        BS.degreeName = "Computer Science";
+        
+        Console.WriteLine("The {0] program contains the {1} degree.", IT.programName, BS.degreeName);
+        Console.WriteLine("The {0] contains the {1}", BS.degreeName, ProgrammingWithC#.name);
+        Console.WriteLine("The {0} has {1} students.", ProgrammingWithC#.name, student.studentsEnrolled);
         }
         
         public void StartEngine(string noiseToMakeWhenStarting) {
@@ -28,29 +47,49 @@ namespace Person
     }
   
   class Student:Person {
-    public override void Drive() {
-        Console.WriteLine("Flying");
-    }
-    public void TakeOff() {
-      Console.WriteLine("Take off");
-    }
-    
-    public void land() {
-      Console.WriteLine("Landing");
+    public void Complain() {
+      Console.WriteLine("When will I ever use this again?!");
     }
   }
   
   class Professor:Person {
-    public override void Drive() {
-        Console.WriteLine("Motoring");
-    }
+    // public override void Drive() {
+    //     Console.WriteLine("Motoring");
+    // }
     
-    public void Accelerate() { 
-        Console.WriteLine("Accelerating"); 
+    public void Done() { 
+        Console.WriteLine("I'm failing you."); 
     } 
-
-    public void Brake() { 
-        Console.WriteLine("Braking"); 
-    }
   }
+  
+      public class DegreeProgram {
+          string programName;
+          string offeredDegree;
+          string programAdmin;
+          Degree BS;
+      }
+
+        public class Degree {
+            string typeOfDegree;
+            string degreeName;
+            int creditsRequired;
+            string course;
+            Course ProgrammingWithC#;
+        }
+        
+        public class Course {
+            string name;
+            string profName;
+            int courseNumber;
+            string days;
+            DateTime start;
+            string[] students = new string[3];
+            string[] instructors = new string[3];
+            
+            Student Huey;
+            Student Dewey;
+            Student Louie;
+            Professor Munsley;
+            // Need a way to increment when a new Student is instanciated.
+          }
 }

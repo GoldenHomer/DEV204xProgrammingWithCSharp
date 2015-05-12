1f37dbac910af68ff808e94fc0a15c13b8b7a18c
Jose Contreras <jocontre@joselcontreras.com>
2015-05-11 21:43:35 -0500

    Update vehicle.cs

--- Module 6/practice/inheritance/vehicle.cs
+++ Module 6/practice/inheritance/vehicle.cs
@@ -28,15 +28,37 @@ namespace Vehicles
         public void StopEngine(string noiseToMakeWhenStopping) {
           Console.WriteLine("Stopping engine: {0}", noiseToMakeWhenStopping);
         }
+        
+        public virtual void Drive() 
+        {
+            Console.WriteLine("Default implementation of the Drive method");
+        }
     }
   
   class Airplane:Vehicle {
+    public override void Drive() {
+        Console.WriteLine("Flying");
+    }
     public void TakeOff() {
       Console.WriteLine("Take off");
     }
     
     public void land() {
       Console.WriteLine("Landing");
+    }
+  }
+  
+  class Car:Vehicle {
+    public override void Drive() {
+        Console.WriteLine("Motoring");
+    }
+    
+    public void Accelerate() { 
+        Console.WriteLine("Accelerating"); 
+    } 
+
+    public void Brake() { 
+        Console.WriteLine("Braking"); 
     }
   }
 }
